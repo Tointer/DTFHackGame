0d1377c21b0bb0da68877e95eb0d4d32004d0ead
Tointer <Tointer@yandex.ru>
2020-07-19 16:03:19 +0300

    2 new levels and update to start screen

--- Assets/StartScreen.cs
+++ Assets/StartScreen.cs
@@ -1,12 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartScreen : MonoBehaviour
 {
+    public Button buttonB;
+    public Image button;
+    public TextMeshProUGUI pressText;
+    public TextMeshProUGUI nameText;
+    public Image clock;
+    
     public void StartGame()
     {
         SceneManager.LoadScene(1);
     }
+
+    public void StartFading()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    IEnumerator FadeOut()
+    {
+        yield return new WaitForSeconds(0.5f);
+        var color = button.color;
+        while (button.color.a > 0)
+        {
+            color = button.color;
+            color.a = Mathf.Clamp01(color.a - Time.deltaTime/6);
+            button.color = color;
+            pressText.color = color;
+            nameText.color = color;
+            clock.color = color;
+            yield return null;
+        }
+
+        buttonB.enabled = false;
+
+    }
+    
+    
 }
--- Assets/StartScreenClock.cs
+++ Assets/StartScreenClock.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class StartScreenClock : MonoBehaviour
 {
     public Transform clockHand;
     private int ticks;
+    public static int activeClocks = 0;
 
     private void Start()
     {
-        ticks = Random.Range(0, 59);
+        activeClocks++;
+        ticks = Random.Range(0, 57);
         clockHand.transform.rotation = Quaternion.AngleAxis(ticks*6 , Vector3.back);
         StartCoroutine(StartClock());
     }
@@ -20,7 +23,30 @@ public class StartScreenClock : MonoBehaviour
         {
             ticks++;
             clockHand.transform.rotation = Quaternion.AngleAxis(6 * ticks, Vector3.back);
+            if (ticks == 60)
+            {
+                StartCoroutine(FadeOut());
+                activeClocks--;
+                if(activeClocks <= 0) GameObject.FindObjectOfType<StartScreen>().StartFading();
+                break;
+            }
             yield return new WaitForSeconds(1);
+        }
+    }
+
+    IEnumerator FadeOut()
+    {
+        var myRend = GetComponent<SpriteRenderer>();
+        var handRend = clockHand.GetComponent<SpriteRenderer>();
+        var color = myRend.color;
+
+        while (myRend.color.a > 0.1)
+        {
+            color = myRend.color;
+            color.a = Mathf.Clamp01(color.a - Time.deltaTime/2);
+            myRend.color = color;
+            handRend.color = color;
+            yield return null;
         }
     }
 }
